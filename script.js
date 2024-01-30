e9550e4ee72c0a2f5d4b8f2a38e1c25a19edc3b6
Niranjan <2100090187@kluniversity.in>
2024-01-30 14:30:16 +0530

    fixed some error

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function generateQRCode() {
       height: 128,
     });
   }
+
+
+
+
+  function htmlEncode(value) {
+    return $('<div/>').text(value)
+      .html();
+  }
+
+  $(function () {
+
+    // Specify an onclick function
+    // for the generate button
+    $('#generate').click(function () {
+
+      // Generate the link that would be
+      // used to generate the QR Code
+      // with the given data 
+      let finalURL =
+'https://chart.googleapis.com/chart?cht=qr&chl=' +
+        htmlEncode($('#content').val()) +
+        '&chs=160x160&chld=L|0'
+
+      // Replace the src of the image with
+      // the QR code image
+      $('.qr-code').attr('src', finalURL);
+    });
+  });
